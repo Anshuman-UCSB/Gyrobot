7ef372c9cd8a49faad77c3bccf0622b06c670e80
Anshuman-UCSB <anshuman@ucsb.edu>
2020-12-25 16:24:51 -0800

    Averaged for gyro

---
+++ gyro.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <wiringPi.h>
+#include "libSensor.h"
+using namespace std;
+
+struct Coord{
+    float x, y, z;
+    Coord():x(0), y(0), z(0){}
+    Coord(float a, float b, float c):x(a), y(b), z(c){}
+    Coord operator+(const Coord& c){
+        Coord out;
+        out.x = x+c.x;
+        out.y = y+c.y;
+        out.z = z+c.z;
+        return out;
+    }
+
+    Coord operator/(int num){
+        Coord c(this);
+        c.x/=num;
+        c.y/=num;
+        c.z/=num;
+        return c;
+    }
+
+    
+};
+
+ostream& operator<<(ostream& os, const Coord& c)
+{
+    os<<"("<<c.x<<", "<<c.y<<", "<<c.z<<")";
+    return os;
+}
+
+Coord getAvg(vector<Coord>& v){
+    Coord t;
+    for(auto& c: v){
+        t+=c;
+    }
+    return c/10;
+}
+
+int main(){
+	if(wiringPiSetupGpio() == -1)
+		return -1;
+	Sensor gyro;
+	printf("Init gyro\n");
+    vector<Coord> v(10);
+    int ind = 0;
+	
+	while(1){
+		v[ind++]=Coord(gyro.getAngleX(),gyro.getAngleY(),gyro.getAngleZ());
+        ind%=10;
+		printf("\033c");
+        cout<<getAvg(v)<<endl;
+
+		delay(1);
+			
+	}
+
+
+}
+
