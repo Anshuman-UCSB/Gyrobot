6155859119693a58392f08e990b4ef9f096ecc17
Anshuman-UCSB <anshuman@ucsb.edu>
2020-12-27 16:49:05 -0800

    Driver class

--- driver.cpp
+++ driver.cpp
@@ -1,23 +1,42 @@
 #include <iostream>
 #include <wiringPi.h>
+#include <thread>
 
 using namespace std;
 
 class Driver{
 	int step[2], dir[2];
+	double speed;
+	int minDelay(0), maxDelay(1000);
+	bool done;
 	Driver(int stepA, int stepB, int dirA, int dirB){
+		done = false;
 		step = {stepA, stepB};
 		dir = {dirA, dirB};
 		for(int i = 0;i<2;i++){
 			pinMode(step[i],OUTPUT);
 			pinMode(dir[i],OUTPUT);
 			digitalWrite(dir[i],1);
-		}		
+		}
+		thread t(&Driver::asyncDriver, this);
+		t.detach();
 	}
 
-	vooid asyncDriver(){
-		digitalWrite(step[i], !digitalRead(step[i]));
-		
+	void asyncDriver(){
+		while(!done){
+			if(speed<0)
+				for(int i = 0;i<2;i++)
+					digitalWrite(dir[i],0);
+			else
+				for(int i = 0;i<2;i++)
+					digitalWrite(dir[i],0);
+			digitalWrite(step[i], !digitalRead(step[i]));
+			delay(abs(speed)*(maxDelay-minDelay));
+		}
+	}
+
+	void kill(){
+		done = true;
 	}
 }
 
