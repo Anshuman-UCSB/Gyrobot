a28f0553d48717abb27ac1cd8f69df3b3faa3cc4
Anshuman-UCSB <anshuman@ucsb.edu>
2020-12-28 20:13:26 -0800

    Gyro class

--- Testing/gyrobot1.cpp
+++ Testing/gyrobot1.cpp
@@ -7,12 +7,12 @@ using namespace std;
 int main(){
 	if(wiringPiSetupGpio() == -1)
 		return -1;
-    Gyro g(100);
+    Gyro g(50);
 	Driver d(14,27,15,17);
 	d.speed = 0;
 	for(;;){
 		float x = g.getGyro().x;
-		d.speed = abs(x)>4?(x>0?80:-80):0;
+		d.speed = abs(x)>4?(x>0?100:-100):0;
 		cout<<x<<endl;
 		delay(10);
 	}
