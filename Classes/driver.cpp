f2b48f4f2411eabdca9abaa9a79589fe5f22ec16
Anshuman-UCSB <anshuman@ucsb.edu>
2020-12-29 19:46:05 -0800

    1/x for function

--- Classes/driver.cpp
+++ Classes/driver.cpp
@@ -40,6 +40,7 @@ class Driver{
 				for(int i = 0;i<2;i++)
 					digitalWrite(step[i], !digitalRead(step[i]));
 			// delay((1-abs(speed))*(maxDelay-minDelay));
+			cout<<"Delay is "<<int(70./speed)<<endl;
 			this_thread::sleep_for(chrono::milliseconds(int(70./speed)));
 
 			// delay(1000);
