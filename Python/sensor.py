dc079aa073aaf5f03fb011b818aa7d584a789767
Anshuman-UCSB <anshuman@ucsb.edu>
2021-01-04 16:36:19 -0800

    Added python bot

--- Python/sensor.py
+++ Python/sensor.py
@@ -7,6 +7,6 @@ l = Logger("http://192.168.0.34:5000/")
 l.delete()
 while True:
     gyro_data = sensor.get_gyro_data()
-    print("x: " + str(gyro_data['x']))
-    l.post(time(), gyro_data['x'])
+    print("y: " + str(gyro_data['y']))
+    l.post(time(), gyro_data['y'])
     # requests.post("http://192.168.0.34:5000/", data = f"{time()},{gyro_data['x']}")
