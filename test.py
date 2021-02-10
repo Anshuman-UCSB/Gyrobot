100e0eba33e6dc56f95a09ce453312e8d3713023
Anshuman-UCSB <anshuman@ucsb.edu>
2021-02-10 13:51:34 -0800

    Update

--- test.py
+++ test.py
@@ -1,15 +1,17 @@
 from mpu6050 import mpu6050
 from time import sleep, time
 from gpiozero import LED
+import threading
 
 sensor = mpu6050(0x68)
 
-left = LED(27)
-right= LED(14)
-leftDir = LED(17)
-leftDir.on()
 
-def step(delay):
+def step(delay, leftPin=27, rightPin = 14, leftDirPin = 17):
+    left = LED(leftPin)
+    right= LED(rightPin)
+    leftDir = LED(leftDirPin)
+    leftDir.on() # set bias
+
     while True:
         start = time()
         while time()-start<delay:
@@ -17,8 +19,17 @@ def step(delay):
         left.toggle()
         right.toggle()
 
-step(.001)
+delay = 1
+p = Thread(target = step, args=(delay))
+p.daemon = True
+p.start()
 
+sleep(2)
+print("changing speed")
+delay = .001
+sleep(5)
+print("DONE")
+exit(1)
 while True:
     data= sensor.get_gyro_data()
     print('\033c')
