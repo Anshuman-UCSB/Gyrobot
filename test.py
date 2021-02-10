6bbfe191e7a2992027bd41a3d86d7a193d26b10a
Anshuman-UCSB <anshuman@ucsb.edu>
2021-02-10 13:34:01 -0800

    makefile

--- test.py
+++ test.py
@@ -1,10 +1,16 @@
 from mpu6050 import mpu6050
 from time import sleep
+from gpiozero import LED
 
 sensor = mpu6050(0x68)
+
+left = LED(27)
+right= LED(14)
 
 while True:
     data= sensor.get_gyro_data()
     print('\033c')
+    left.toggle()
+    right.toggle()
     print(data['y'])
     sleep(.1)
