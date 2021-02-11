f6e624d0fdaf891aa6210dfe374e29eb0a1957d6
Anshuman-UCSB <anshuman@ucsb.edu>
2021-02-10 21:48:51 -0800

    Update

--- test.py
+++ test.py
@@ -62,8 +62,8 @@ while True:
     if abs(dyAvg)>1:
         delay[0] = .01/abs(dyAvg)
         while delay[0] >=.001:
-            delay -=.01
-            delay/=1.1
+            delay[0] -=.01
+            delay[0]/=1.1
             sleep(.1)
 
     else:
